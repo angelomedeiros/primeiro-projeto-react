7a0f2d1a52ec44013649ab8ff07e975f7d7e3de0
Angelo Medeiros <angelomedeiros3@gmail.com>
2017-12-08 00:31:25 -0300

    Configuracao do hot loader

---
+++ react-webpack/src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+import createReactClass from 'create-react-class'
+
+const App = createReactClass({
+  render: function () {
+    return <h2>Aplicação</h2>
+  }
+})
+
+export default App
